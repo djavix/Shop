87c5244b7755e43186b29f4e671a030799fe3e92
jvelasquez <jvelasquez@pradotec-america.com>
2020-01-13 17:07:39 -0400

    MOdificando la clase Product y realizando cambios para la vista y la base de datos.

---
+++ Shop.Web/Migrations/20200113204327_ModifyProducts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Web.Migrations
+{
+    public partial class ModifyProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Produtcs",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastSale",
+                table: "Produtcs",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastPurchase",
+                table: "Produtcs",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Produtcs",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastSale",
+                table: "Produtcs",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastPurchase",
+                table: "Produtcs",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
