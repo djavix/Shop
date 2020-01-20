4995c21d3c03ccc54fbada0bdaed8643d064af44
jvelasquez <jvelasquez@pradotec-america.com>
2020-01-20 11:05:37 -0400

    Agregado:  -Manejo de date pickers en ordenes para el Deliver -Manejo de datepicker para create y edit de product

--- Shop.Web/Data/Repositories/IOrderRepository.cs
+++ Shop.Web/Data/Repositories/IOrderRepository.cs
@@ -19,6 +19,11 @@ namespace Shop.Web.Data.Repositories
         Task DeleteDetailTempAsync(int id);
 
         Task<bool> ConfirmOrderAsync(string userName);
+
+        Task DeliverOrder(DeliverViewModel model);
+
+        Task<Order> GetOrdersAsync(int id);
+
     }
 
 }
---
+++ Shop.Web/Models/DeliverViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Web.Models
+{
+    public class DeliverViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Delivery date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DeliveryDate { get; set; }
+    }
+
+}
